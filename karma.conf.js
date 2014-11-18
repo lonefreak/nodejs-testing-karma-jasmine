387c18f3f9dcd404f47d1d5488394f92aca47f83
Fabricio Leotti <fabricio.leotti@wal-mart.com>
2014-11-17 22:52:13 -0200

    Fixing code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress','coverage'],
 
-    coverageReporter = {
+    coverageReporter: {
         type : 'lcovonly',
         dir : 'coverage/',
     },
